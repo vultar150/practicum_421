53326a7a9c7f7294325b538d9192a1967862c9a9
Ryabchenkov V. M <vultar1500@gmail.com>
2020-11-05 13:59:07 +0300

    delete some debug output and log function

--- task2_simulated_annealing/src/main.cpp
+++ task2_simulated_annealing/src/main.cpp
@@ -1,6 +1,3 @@
-#include</Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/math.h>
-#include</Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/__cxx_version>
-
 #include <iostream>
 #include <memory>
 #include <unordered_map>
@@ -15,18 +12,16 @@
 #include "mutation.h"
 #include "sequential.h"
 
-
-using SPtrAbstractData = std::shared_ptr<AbstractTypeDecision<MyDataType>>;
+#include "log.h"
 
 
 int main(int argc, char** argv)
 {
-    std::unordered_map<std::string, int> params;
+    ParamsT params;
     setParameters(params);
 
     SPtrAbstractData bestSolution(new TypeDecision(argv[1]));
     std::vector<SPtrAbstractData> records(params["NUM_OF_THREADS"], bestSolution);
-
     int idBestRecord = 0, it = 0;
 
     omp_set_num_threads(params["NUM_OF_THREADS"]);
@@ -34,36 +29,16 @@ int main(int argc, char** argv)
 
     while(it < params["NUM_OF_IT_OUTER_MOST_LOOP_WITHOUT_CHANGE"])
     {
-        // std::cout << std::endl;
-        // std::cout << std::endl;
-        // std::cout << "############ NEXT IT OF MULTITHREAD ##################" << std::endl;
-        // std::cout << "IT WITHOUT CHANGE = " << it << std::endl;
         #pragma omp parallel shared(records, bestSolution, params)
         {
-            // #pragma omp critical
-            // {
-            //     bestSolution->print();
-            // }
-            // std::cout << "TUT!!!!" << std::endl << std::endl << std::endl;
             SequentialAlg<TypeDecision, MyOperation,
-                          CauchyLaw, MyDataType> algorithm(bestSolution, params);
+                          ThirdLaw, MyDataType> algorithm(bestSolution, params);
             #pragma omp critical
             {
                 algorithm.print();
             }
-            // std::cout << "NUM THREADS = " << omp_get_num_threads() << std::endl;
             int threadNum = omp_get_thread_num();
             records[threadNum] = algorithm.startMainCycle();
-            // #pragma omp critical
-            // {
-            //     std::cout << "//////////////////////////////####!!!!!!!!!!!!" << std::endl;
-            //     std::cout << "THREAD = " << threadNum << std::endl;
-            //     std::cout << "TARGET VALUE = " << records[threadNum]->targetFunc() << std::endl;
-            //     std::cout << "EXECUTION TIME (SUM) = " << records[threadNum]->getExecTime() << std::endl;
-            //     std::cout << "ID MAX PROC = " << records[threadNum]->getIdMaxProc() << std::endl;
-            //     std::cout << "ID MIN PROC = " << records[threadNum]->getIdMinProc() << std::endl;
-            //     std::cout << "//////////////////////////////####!!!!!!!!!!!!" << std::endl;
-            // }
             #pragma omp barrier
         }
         int minOrMax = records[idBestRecord=0]->targetFunc();
@@ -81,43 +56,11 @@ int main(int argc, char** argv)
             it++;
         else it = 0;
         bestSolution = records[idBestRecord];
-        // std::cout << "BEST SOLUTION: " << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-        // // std::cout << "THREAD = " << threadNum << std::endl;
-        // std::cout << "TARGET VALUE = " << bestSolution->targetFunc() << std::endl;
-        // std::cout << "EXECUTION TIME (SUM) = " << bestSolution->getExecTime() << std::endl;
-        // std::cout << "ID MAX PROC = " << bestSolution->getIdMaxProc() << std::endl;
-        // std::cout << "ID MIN PROC = " << bestSolution->getIdMinProc() << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-        // std::cout << "//////////////////////////////" << std::endl;
-
-        // std::cout << "############ END IT OF MULTITHREAD ##################" << std::endl;
-        // std::cout << std::endl;
-        // std::cout << std::endl;
     }
 
     auto end = std::chrono::steady_clock::now();
     auto timeDiff = end - start;
-    // std::shared_ptr<AbstractTypeDecision<MyDataType>> record = algorithm.startMainCycle();
-    std::ofstream log;
-    log.open("log.txt", std::ios::app);
-    if (log.is_open())
-    {
-        log << "DATA FILE = " << argv[1] << std::endl;
-        log << "NUM OF THREADS = " << params["NUM_OF_THREADS"] << std::endl;
-        log << "\tINITIAL TEMPERATURE = " << params["INITIAL_TEMPERATURE"] << std::endl;
-        log << "\tNUM OF IT OUTER MOST LOOP WITHOUT CHANGE = " << params["NUM_OF_IT_OUTER_MOST_LOOP_WITHOUT_CHANGE"] << std::endl;
-        log << "\tNUM OF IT INNER MOST LOOP WITHOUT CHANGE = " << params["NUM_OF_IT_INNER_MOST_LOOP_WITHOUT_CHANGE"] << std::endl;
-        log << "\tNUM OF IT WITHOUT CHANGE TEMPERATUE = " << params["NUM_OF_IT_WITHOUT_CHANGE_TEMPERATUE"] << std::endl;
-        log << "\t\tTARGET VALUE = " << bestSolution->targetFunc() << std::endl;
-        log << "\t\tEXECUTION TIME (SUM) = " << bestSolution->getExecTime() << std::endl;
-        log << "\t\t" << std::chrono::duration <double, std::milli> (timeDiff).count() << " ms" << std::endl;
-    }
-    log.close();
-    // record->print();
+    log(argv[1], bestSolution, params, timeDiff);
+    // bestSolution->print();
     return 0;
 }

1f0f914f44835db68e780f7b6a2f76c390986173
Ryabchenkov V. M <vultar1500@gmail.com>
2020-11-05 13:53:50 +0300

    add log function

---
+++ task2_simulated_annealing/src/log.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <fstream>
+#include <chrono>
+
+#include "XDecision.h"
+#include "log.h"
+
+
+void 
+log(char* fileName,  SPtrAbstractData bestSolution,   
+    ParamsT& params, std::chrono::duration<double> time)
+{
+    std::ofstream log;
+    log.open("log.txt", std::ios::app);
+    if (log.is_open())
+    {
+        log << "DATA FILE = " << fileName << std::endl;
+        log << "NUM OF THREADS = " << params["NUM_OF_THREADS"] << std::endl;
+        log << "\tINITIAL TEMPERATURE = " 
+            << params["INITIAL_TEMPERATURE"] << std::endl;
+        log << "\tNUM OF IT OUTER MOST LOOP WITHOUT CHANGE = " 
+            << params["NUM_OF_IT_OUTER_MOST_LOOP_WITHOUT_CHANGE"] << std::endl;
+        log << "\tNUM OF IT INNER MOST LOOP WITHOUT CHANGE = " 
+            << params["NUM_OF_IT_INNER_MOST_LOOP_WITHOUT_CHANGE"] << std::endl;
+        log << "\tNUM OF IT WITHOUT CHANGE TEMPERATUE = " 
+            << params["NUM_OF_IT_WITHOUT_CHANGE_TEMPERATUE"] << std::endl;
+        log << "\t\tTARGET VALUE = " << bestSolution->targetFunc() << std::endl;
+        log << "\t\tEXECUTION TIME (SUM) = " 
+            << bestSolution->getExecTime() << std::endl;
+        log << "\t\t" 
+            << std::chrono::duration <double, std::milli> (time).count() 
+            << " ms" << std::endl;
+    }
+    log.close();
+}
---
+++ task2_simulated_annealing/src/log.h
@@ -0,0 +1,13 @@
+#ifndef LOG_H
+#define LOG_H
+
+
+using SPtrAbstractData = std::shared_ptr<AbstractTypeDecision<MyDataType>>;
+using ParamsT = std::unordered_map<std::string, int>;
+
+void 
+log(char* fileName,  SPtrAbstractData bestSolution, 
+    ParamsT& params, std::chrono::duration<double> time);
+
+
+#endif // LOG_H

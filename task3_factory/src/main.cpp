64a06935f70a3cad152897ea5f2c944ff3ef6f04
Vladimir Ryabchenkov <vladimir@Vladimirs-MacBook-Pro.local>
2020-12-01 05:09:28 +0300

    add all operations and logic error exceptions

--- task3_factory/src/main.cpp
+++ task3_factory/src/main.cpp
@@ -6,58 +6,104 @@
 
 int main(int argc, char **argv)
 {
-    FuncFactory factory;
-    // std::vector<IFunctionPtr> cont;
-    auto functions = factory.getAvailableFunctions();
-    for (const auto& func : functions)
-    {
-        std::cout << func << std::endl;
+    try {
+        FuncFactory factory;
+        // std::vector<IFunctionPtr> cont;
+        auto functions = factory.getAvailableFunctions();
+        for (const auto& func : functions)
+        {
+            std::cout << func << std::endl;
+        }
+        std::cout << std::endl;
+        auto ff = factory.createFunction("power", 2);
+        std::cout << ff->toString() << "  for x = 2 is " << (*ff)(2) << std::endl;
+        std::cout << ff->toString() << "  for x = 4 is " << (*ff)(4) << std::endl;
+        std::cout << "derive for x = 4 is " << (*ff).getDerive(4) << std::endl;
+        std::cout << std::endl;
+
+        auto gg = factory.createFunction("polynomial", {7, 0, 3, 15});
+
+        std::cout << ff->toString() << "  for x = 10 is " << (*ff)(10) << std::endl;
+        std::cout << gg->toString() << "  for x = 10 is " << (*gg)(10) << std::endl;
+
+        std::cout << std::endl;
+        auto fConst = factory.createFunction("const", 2.);
+        std::cout << fConst->toString() << "  for x = 7 is " << (*fConst)(7) << std::endl;
+        std::cout << "derive for x = 64 is " << fConst->getDerive(64) << std::endl;
+
+        auto fPolynomial = factory.createFunction("polynomial", {1, 0, 3});
+        std::cout << fPolynomial->toString() << "  for x = 2 is " << (*fPolynomial)(2) << std::endl;
+        std::cout << "derive for x = 2 is " << fPolynomial->getDerive(2) << std::endl;
+
+        auto fPolynomial2 = factory.createFunction("polynomial", {1, 1, 2});
+        std::cout << fPolynomial2->toString() << "  for x = 2.2 is " << (*fPolynomial2)(2.2) << std::endl;
+        std::cout << "derive for x = 2.2 is " << fPolynomial2->getDerive(2.2) << std::endl;
+
+
+        auto fExp = factory.createFunction("exp");
+        std::cout << fExp->toString() << "  for x = 2.5 is " << (*fExp)(2.5) << std::endl;
+        std::cout << "derive for x = 2.5 is " << fExp->getDerive(2.5) << std::endl;
+
+        auto fIdent = factory.createFunction("ident");
+        std::cout << fIdent->toString() << "  for x = 5.5 is " << (*fIdent)(5.5) << std::endl;
+        std::cout << fIdent->toString() << "  for x = 5 is " << (*fIdent)(5) << std::endl;
+        std::cout << "derive for x = 5.5 is " << fIdent->getDerive(5.5) << std::endl;
+
+        auto fPolynomial3 = factory.createFunction("polynomial", {0, 0, 0, 1., 0, 2.33, 0, 0});
+        std::cout << fPolynomial3->toString() << "  for x = 2.2 is " << (*fPolynomial3)(2.2) << std::endl;
+        std::cout << "derive for x = 2.2 is " << fPolynomial3->getDerive(2.2) << std::endl;
+        std::cout << std::endl;
+
+        Polynomial* pl = new Polynomial({1, 2, 3 , 4});
+        Polynomial* fPolynomial4 = new Polynomial(*pl);
+        std::cout << fPolynomial4->toString() << "  for x = 2.2 is " << (*fPolynomial4)(2.2) << std::endl;
+        std::cout << "derive for x = 2.2 is " << fPolynomial4->getDerive(2.2) << std::endl;
+        std::cout << std::endl;
+
+        auto sum = *fPolynomial3 + *fExp;
+        std::cout << fPolynomial3->toString() << "  for x = 2 is " << (*fPolynomial3)(2) << std::endl;
+        std::cout << fExp->toString() << "  for x = 2 is " << (*fExp)(2) << std::endl;
+        std::cout << sum.toString() << "  for x = 2 is " << sum(2) << std::endl;
+        std::cout << "derive for x = 2 is " << sum.getDerive(2) << std::endl;
+
+        auto sum2 = *fExp + *fIdent;
+        std::cout << fExp->toString() << "  for x = 2 is " << (*fExp)(2) << std::endl;
+        std::cout << sum2.toString() << "  for x = 2 is " << sum2(2) << std::endl;
+        std::cout << "derive for x = 2 is " << sum2.getDerive(2) << std::endl;
+        std::cout << "///////// TESTING MULTYPLY ////////" << std::endl;
+        auto mul = (*fExp) * (*fIdent);
+        std::cout << fExp->toString() << "  for x = 2 is " << (*fExp)(2) << std::endl;
+        std::cout << mul.toString() << "  for x = 2 is " << mul(2) << std::endl;
+        std::cout << "derive for x = 2 is " << mul.getDerive(2) << std::endl;
+
+        // std::string str = std::string("sdsd");
+        auto mul2 = *fConst + mul * (*fPolynomial);
+        std::cout << fPolynomial->toString() << "  for x = 2 is " << (*fPolynomial)(2) << std::endl;
+        std::cout << mul2.toString() << "  for x = 2 is " << mul2(2) << std::endl;
+        std::cout << "derive for x = 2 is " << mul2.getDerive(2) << std::endl;
+
+        std::cout << "///////// TESTING DIVIDE ////////" << std::endl;
+
+        auto div1 = mul2 / (*fPolynomial3);
+        std::cout << mul2.toString() << "  for x = 2 is " << mul2(2) << std::endl;
+        std::cout << "derive for x = 2 is " << mul2.getDerive(2) << std::endl << std::endl;
+
+        std::cout << fPolynomial3->toString() << "  for x = 2 is " << (*fPolynomial3)(2) << std::endl;
+        std::cout << "derive for x = 2 is " << fPolynomial3->getDerive(2) << std::endl;
+    
+        std::cout << "///////// RESULT ////////" << std::endl;
+        std::cout << div1.toString() << "  for x = 2 is " << div1(2) << std::endl;
+        std::cout << "derive for x = 2 is " << div1.getDerive(2) << std::endl;
     }
-    std::cout << std::endl;
-    auto ff = factory.createFunction("power", 2);
-    std::cout << ff->toString() << " for x = 2 is " << (*ff)(2) << std::endl;
-    std::cout << ff->toString() << " for x = 4 is " << (*ff)(4) << std::endl;
-    std::cout << "derive for x = 4 is " << (*ff).getDerive(4) << std::endl;
-    std::cout << std::endl;
-
-    auto gg = factory.createFunction("polynomial", {7, 0, 3, 15});
-
-    std::cout << ff->toString() << " for x = 10 is " << (*ff)(10) << std::endl;
-    std::cout << gg->toString() << " for x = 10 is " << (*gg)(10) << std::endl;
-
-    std::cout << std::endl;
-    auto fConst = factory.createFunction("const", 2.);
-    std::cout << fConst->toString() << " for x = 7 is " << (*fConst)(7) << std::endl;
-    std::cout << "derive for x = 64 is " << fConst->getDerive(64) << std::endl;
-
-    auto fPolynomial = factory.createFunction("polynomial", {1, 0, 3});
-    std::cout << fPolynomial->toString() << " for x = 2 is " << (*fPolynomial)(2) << std::endl;
-    std::cout << "derive for x = 2 is " << fPolynomial->getDerive(2) << std::endl;
-
-    auto fPolynomial2 = factory.createFunction("polynomial", {1, 1, 2});
-    std::cout << fPolynomial2->toString() << " for x = 2.2 is " << (*fPolynomial2)(2.2) << std::endl;
-    std::cout << "derive for x = 2.2 is " << fPolynomial2->getDerive(2.2) << std::endl;
-
-
-    auto fExp = factory.createFunction("exp");
-    std::cout << fExp->toString() << " for x = 2.5 is " << (*fExp)(2.5) << std::endl;
-    std::cout << "derive for x = 2.5 is " << fExp->getDerive(2.5) << std::endl;
-
-    auto fIdent = factory.createFunction("ident");
-    std::cout << fIdent->toString() << " for x = 5.5 is " << (*fIdent)(5.5) << std::endl;
-    std::cout << fIdent->toString() << " for x = 5 is " << (*fIdent)(5) << std::endl;
-    std::cout << "derive for x = 5.5 is " << fIdent->getDerive(5.5) << std::endl;
-
-    auto fPolynomial3 = factory.createFunction("polynomial", {0, 0, 0, 1., 0, 2.33, 0, 0});
-    std::cout << fPolynomial3->toString() << " for x = 2.2 is " << (*fPolynomial3)(2.2) << std::endl;
-    std::cout << "derive for x = 2.2 is " << fPolynomial3->getDerive(2.2) << std::endl;
-    std::cout << std::endl;
-
+    catch (std::logic_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << typeid(err).name() << std::endl;
+    }
     // *fPolynomial4 = *fPolynomial3;
-    // std::cout << fPolynomial4->toString() << " for x = 2.2 is " << (*fPolynomial4)(2.2) << std::endl;
+    // std::cout << fPolynomial4->toString() << "  for x = 2.2 is " << (*fPolynomial4)(2.2) << std::endl;
     // std::cout << "derive for x = 2.2 is " << fPolynomial4->getDerive(2.2) << std::endl;
     // std::cout << std::endl;
-    // std::cout << fPolynomial3->toString() << " for x = 2.2 is " << (*fPolynomial3)(2.2) << std::endl;
+    // std::cout << fPolynomial3->toString() << "  for x = 2.2 is " << (*fPolynomial3)(2.2) << std::endl;
     // std::cout << "derive for x = 2.2 is " << fPolynomial3->getDerive(2.2) << std::endl;
     return 0;
 }
